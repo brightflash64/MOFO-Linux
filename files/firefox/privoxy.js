72055c241e3b6a9b5606c0a6c31b996b5706713e
brightflash64 <wm_munny92@yahoo.com>
2016-04-22 06:11:08 +0000

    Added files via upload
    Global Firefox configuration for Privoxy.

---
+++ files/firefox/privoxy.js
@@ -0,0 +1,11 @@
+//set privoxy as default
+user_pref("network.proxy.ftp", "127.0.0.1");
+user_pref("network.proxy.ftp_port", 8118);
+user_pref("network.proxy.http", "127.0.0.1");
+user_pref("network.proxy.http_port", 8118);
+user_pref("network.proxy.share_proxy_settings", true);
+user_pref("network.proxy.socks", "127.0.0.1");
+user_pref("network.proxy.socks_port", 8118);
+user_pref("network.proxy.ssl", "127.0.0.1");
+user_pref("network.proxy.ssl_port", 8118);
+user_pref("network.proxy.type", 1);
