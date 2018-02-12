8c74b34a051b2d09286e45f14df11013a0469ac0
brightflash64 <wm_munny92@yahoo.com>
2018-02-12 00:20:22 +0000

    Update bitmask-controller.sh

--- files/scripts/bitmask-controller.sh
+++ files/scripts/bitmask-controller.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# Bitmask-controller.
 # Copyright (c) 2018 by Philip Collier, <webmaster@mofolinux.com>
 # This script is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
