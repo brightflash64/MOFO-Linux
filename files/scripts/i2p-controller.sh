1f2e6259bddb4f471cd486efc7efb22c2fd1a6f8
brightflash64 <wm_munny92@yahoo.com>
2018-02-12 00:23:14 +0000

    Update i2p-controller.sh

--- files/scripts/i2p-controller.sh
+++ files/scripts/i2p-controller.sh
@@ -1,21 +1,34 @@
 #!/bin/sh
+
+# Copyright (c) 2018 by Philip Collier, <webmaster@mofolinux.com>
+# This script is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version. There is NO warranty; not even for
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
 Encoding=UTF-8
 
 i2pstart(){
-	systemctl stop privoxy.service
+	sudo /etc/init.d/privoxy stop
 	sudo cp /usr/local/etc/privoxy/config.i2p /usr/local/etc/privoxy/config
-	systemctl start privoxy.service
+	sudo /etc/init.d/privoxy start
 	/usr/bin/i2prouter start;
 }
 
 i2pstop(){
-	systemctl stop privoxy.service
-	sudo cp /usr/local/etc/privoxy/config.orig /usr/local/etc/privoxy/config
-	systemctl start privoxy.service
 	/usr/bin/i2prouter stop;
+	sudo /etc/init.d/privoxy stop
+	sudo cp /usr/local/etc/privoxy/config.orig /usr/local/etc/privoxy/config
+	sudo /etc/init.d/privoxy start
+sudo bash -c 'echo "nameserver 127.0.2.1
+nameserver 127.0.2.2" > /run/resolvconf/resolv.conf'
+	systemctl restart dnscrypt-proxy
+	systemctl restart dnscrypt-proxy2
+	exit
 }
 
-ans=$(zenity  --list  --title "I2P CONTROLLER" --text "Start and stop I2P" --radiolist  --column "Pick" --column "Action" TRUE "Start I2P" FALSE "Stop I2P");
+ans=$(zenity  --list  --title "I2P CONTROLLER" --text "Start and stop I2P" --radiolist  --column "Pick" --column "Action" FALSE "Start I2P" TRUE "Stop I2P");
 
 	if [  "$ans" = "Start I2P" ]; then
 		i2pstart
