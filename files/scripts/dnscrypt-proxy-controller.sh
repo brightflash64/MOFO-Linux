e9cddb4261019032499da72f9ef40c01da081feb
brightflash64 <wm_munny92@yahoo.com>
2018-02-12 00:15:36 +0000

    Update dnscrypt-proxy-controller.sh

--- files/scripts/dnscrypt-proxy-controller.sh
+++ files/scripts/dnscrypt-proxy-controller.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
+
+# DNScrypt-proxy-controller.
+# Copyright (c) 2018 by Philip Collier, <webmaster@mofolinux.com>
+# This script is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version. There is NO warranty; not even for
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
 Encoding=UTF-8
 
 dnscryptrestart(){
-echo "nameserver 127.0.2.1
-nameserver 127.0.2.2" > /run/resolvconf/resolv.conf
+sudo bash -c 'echo "nameserver 127.0.2.1
+nameserver 127.0.2.2" > /run/resolvconf/resolv.conf'
 systemctl restart dnscrypt-proxy
 systemctl restart dnscrypt-proxy2
 }
 
 dnscryptstop(){
-echo "nameserver 8.8.8.8
-nameserver 8.8.4.4" > /run/resolvconf/resolv.conf
+sudo bash -c 'echo "nameserver 9.9.9.9
+nameserver 8.8.8.8" > /run/resolvconf/resolv.conf'
 systemctl stop dnscrypt-proxy
 systemctl stop dnscrypt-proxy2
 }
